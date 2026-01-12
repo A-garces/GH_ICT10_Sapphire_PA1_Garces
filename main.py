8df38bc47de0af8bdc694d3ceab8ed449ba2ef89
A-garces <garces.adm@obmontessori.edu.ph>
2026-01-12 11:19:07 +0800

    Update fever threshold from 38 to 37.8 degrees Celsius

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ def converting(e):
     display(f'Fahrenheit: {ftem}', target="fever", append="False")
     display(f'Celsius: {final}', target="fever", append="False")
 
-    if ctem >= 38:
+    if ctem >= 37.8:
         display('You have a fever, please consult a doctor.', target="fever", append="False")
 
     elif ctem <= 10:
         display('Please input proper temperature, unless you are in a freezer.', target="fever", append="False")
     
     else:
+
         display('Status: Normal, continue with a healthy diet.', target="fever", append="False")
