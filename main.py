b9ce5b30d8c74f765564b2f061d14fd825668680
A-garces <garces.adm@obmontessori.edu.ph>
2026-01-12 11:16:26 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from pyscript import document, display
+
+def converting(e):
+
+    ftem = float(document.getElementById('tem').value)
+    
+    ctem = (ftem - 32) * 5/9
+    final = round(ctem, 0)
+
+    output = document.getElementById("output")
+    output.innerHTML = ""
+
+    display(f'Fahrenheit: {ftem}', target="fever", append="False")
+    display(f'Celsius: {final}', target="fever", append="False")
+
+    if ctem >= 38:
+        display('You have a fever, please consult a doctor.', target="fever", append="False")
+
+    elif ctem <= 10:
+        display('Please input proper temperature, unless you are in a freezer.', target="fever", append="False")
+    
+    else:
+        display('Status: Normal, continue with a healthy diet.', target="fever", append="False")
